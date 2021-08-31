bbb9ab71848f06e7ae2efab1d97c22581375639b
gmendozah <geovanimendoza.h@gmail.com>
2021-08-30 23:36:37 -0400

    feat: add initial code

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 import math
 
 
-def eit(x):
-    if x <= 0 or x - x > 0:
+def eit(number):
+    if number <= 0 or number - int(number) > 0:
         return 0
     else:
         return 1
 
 
-xrange = range
-
 # x = pow(10, 25)
-x = 100
+x = 1000
 
-aa = (2 * x + pow(-1, x - 1)) / 6
-a = math.trunc(aa)
+aa = (2 * x + pow(-1, x) - 1) / 6
+a = round(aa, 0)
 
 cc = ((-1 + (math.sqrt(-2 + 3 * aa))) / 3)
-c = -math.trunc((cc * (-1)))
+c = -round(cc * -1, 0)
 m = c
 n = a - 1
-sum = 0
+s = 0
 k = 0
 t = 0
 
-for j in range(8, n):
-    for i in range(1, m):
-        r = eit((4 * j - ((-1) ^ j) + ((2 * i + 1) * pow(-1, i + j) + ((2 * i - 1) * pow(-1, i) - (12 * i * i) + 5) / (
-                12 * i + 6 - (2 * pow(-1, i))))))
+for j in range(8, int(n)):
+    for i in range(1, int(m)):
+        r = eit(
+            (4 * j - pow(-1, j) + ((2 * i + 1) * pow(-1, i + j)) + ((2 * i - 1) * pow(-1, i)) - (12 * i * i) + 5) / (
+                    12 * i + 6 - (2 * pow(-1, i))))
         k = k + r
-    sum = sum + eit(k)
+    s = s + eit(k)
     k = 0
 
-pp = (2 * x + pow(-1, x) - 6 * sum + 5) / 6
+pp = (2 * x + pow(-1, x) - 6 * s + 5) / 6
 p = math.trunc(pp)
 print("valor de pi(x) = ", p)
